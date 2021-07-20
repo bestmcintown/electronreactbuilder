d2f9bc677ef02d39e10159fb34ddcde20d72e3c8
bestmcintown <bestmcintown@protonmail.com>
2021-07-20 09:42:19 -0400

    electronReactBuilder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+const fs = window.require("fs");
+const { app } = window.require("@electron/remote");
 
 function App() {
+  console.log("test");
+  const [path, setPath] = useState(app.getAppPath());
+
+  const files = fs.readdirSync(path);
+  console.log(files);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> cocacola and save to reload.
         </p>
+        <pre>{JSON.stringify(files, null, 2)}</pre>
         <a
           className="App-link"
           href="https://reactjs.org"
